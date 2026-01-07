04c2a32d3527eeacf6458dad06fd1fb062279d96
anayush87 <150177653+anayush87@users.noreply.github.com>
2026-01-07 10:40:42 +0530

    Time: 1 ms (17.77%), Space: 46.6 MB (9.69%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,31 +1,47 @@
 class Solution {
-    Integer dp[][];
+    int dp[][];
     public int maxProfit(int[] prices) {
-        dp=new Integer[prices.length][2];
-        return solve(0,1,prices);
+        int n=prices.length;
+        dp=new int[n+1][2];
+        int profit=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<2;j++)
+            {
+                if(j==0)
+                {
+                    dp[i][j]=Math.max(-prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+                else{
+                    dp[i][j]=Math.max(prices[i]+dp[i+1][0],dp[i+1][1]);
+                }
+                
+            }
+        }
+        return dp[0][0];
     }
-    public int solve(int i,int canBuy,int prices[])
-    {
-        if(i>=prices.length)
-        {
-            return 0;
-        }
-        if(dp[i][canBuy]!=null) return dp[i][canBuy];
-        int buy;
-        int skip;
-        int sell;
-        if(canBuy==1)
-        {
-            buy=-prices[i]+solve(i+1,0,prices);
-            skip=solve(i+1,1,prices);
-            return dp[i][canBuy]=Math.max(buy,skip);
-        }
-        else
-        {
-            sell=prices[i]+solve(i+1,1,prices);
-            skip=solve(i+1,0,prices);
-            return dp[i][canBuy]=Math.max(sell,skip);
-        }
+    // public int solve(int i,int canBuy,int prices[])
+    // {
+    //     if(i>=prices.length)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i][canBuy]!=null) return dp[i][canBuy];
+    //     int buy;
+    //     int skip;
+    //     int sell;
+    //     if(canBuy==1)
+    //     {
+    //         buy=-prices[i]+solve(i+1,0,prices);
+    //         skip=solve(i+1,1,prices);
+    //         return dp[i][canBuy]=Math.max(buy,skip);
+    //     }
+    //     else
+    //     {
+    //         sell=prices[i]+solve(i+1,1,prices);
+    //         skip=solve(i+1,0,prices);
+    //         return dp[i][canBuy]=Math.max(sell,skip);
+    //     }
         
-    }
+    // }
 }
